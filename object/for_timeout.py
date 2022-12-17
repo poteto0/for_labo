03dd7c45944fb395edf5ac63ea84b7e6ab5ece94
poteto <67512297+poteto0@users.noreply.github.com>
2022-12-17 19:47:03 +0900

    Delete for_timeout.py

--- object/for_timeout.py
+++
@@ -1,9 +0,0 @@
-import time
-import pyautogui as pgui
-
-for _ in range(60):
-    time.sleep(600) # 10分待機
-    pgui.click(x=652, y=703, duration=1) # 指定した座標に1秒かけて移動してクリッ
-ク
-    time.sleep(1) # 1秒待機
-    pgui.click(x=664, y=638, duration=1)
